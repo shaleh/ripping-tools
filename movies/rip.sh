23676c930ea408a2db06119c41537c77a2966aab
Sean Perry <shaleh@speakeasy.net>
2016-04-08 14:22:27 -0700

    Add movie scripts

---
+++ movies/rip.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BASE=/srv/multimedia/video/
+
+dir=$1
+shift
+dest=$(echo "$*" | sed -e 's/ /_/g')
+if [ ! -d "$BASE/$dir" ]; then
+	echo "Error: $dir does not exist"
+	exit 1
+fi
+output="$BASE/$dir/${dest}.mp4"
+if [ -f "$output" ]; then
+	echo "Error: $output already exists"
+	exit 2
+fi
+echo $output
+
+begin=`date +"%s"`
+HandBrakeCLI -i /dev/dvd --main-feature -O --preset Normal -E copy -o "$output"
+end=`date +"%s"`
+
+duration=$((end - begin))
+echo "Elapsed: $((duration / 60)) minutes"
+eject
---
+++ movies/rip_within_track.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -ne 6 ]; then
+	>&2 echo "Usage: $0 title ch1 ch2 first_chapter chapter_length cmdfile"
+	exit 1
+fi
+
+title=$1
+ch1=$2
+chN=$3
+initial=$4  # First chapter
+length=$5  # how many chapters per item?
+cmdfile=$6
+
+device=/dev/dvd
+
+for i in $(seq $ch1 $chN)
+do
+        # Need the minus 1 because 2 - 6 is 5 items not 4.
+        last=$((initial + $length - 1))
+	echo "Starting $i"
+	echo "$initial to $last"
+	index=$i
+	. $cmdfile
+	initial=$((last + 1))
+done
+
+eject $device
